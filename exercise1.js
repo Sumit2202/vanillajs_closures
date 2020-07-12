6475dd16c8671dd747ac6626a0b2e896b166f218
Sumit <sumit22mittal@gmail.com>
2020-07-12 12:42:31 -0400

    added ex1 and ex2
    added ex1 and ex2

--- exercise1.js
+++ exercise1.js
@@ -1,5 +1,8 @@
 /* Challenge 1
-Create a function createFunction that creates and returns a function. When that created function is called, it should print "hello". When you think you completed createFunction, un-comment out those lines in the code and run it to see if it works. 
+Create a function createFunction that creates and returns a function.
+When that created function is called, it should print "hello".
+When you think you completed createFunction, un-comment out those
+lines in the code and run it to see if it works. 
 */
 
 function createFunction() {
